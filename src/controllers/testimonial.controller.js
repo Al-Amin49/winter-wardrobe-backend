bdcddf4373cec02c633873572f6e9dc0779b2625
Al-Amin49 <alamin496832@gmail.com>
2024-08-01 23:35:31 +0600

    fix testimonial model

--- src/controllers/testimonial.controller.js
+++ src/controllers/testimonial.controller.js
@@ -4,53 +4,40 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 /***
-@desc     create testimonial
+@desc     Create testimonial
 @route    POST api/v1/testimonial/
-@access  private
- */
-
+@access   Private
+*/
 const postTestimonial = asyncHandler(async (req, res) => {
-  const { username, message, location, rating } = req.body;
+  const testimonialData =req.body;
 
-  //validate the input
-  // if (!username ||  !message || !location || !rating === undefined) {
-  //   throw new ApiError(400, "All fields are required");
+  // Validate request body
+  // if (!userId || !clotheId || !quantity) {
+  //   throw new ApiError("All fields are required", 400);
   // }
-  //ensure the rating range is within the allowed range
-  if (rating < 1 || rating > 5) {
-    throw new ApiError(400, "Rating must be between 1 and 5");
-  }
 
-  const newTestimonail = await Testimonial.create({
-    username,
-    message,
-    location,
-    rating,
-  });
+  const newTestimonial = await Testimonial.create(testimonialData);
+
   res
     .status(201)
-    .json(
-      new ApiResponse(200, newTestimonail, "Testimonial added successfully")
-    );
+    .json(new ApiResponse(200, newTestimonial, "Testimonial added successfully"));
 });
+
+
 /**
  * @desc    Get all testimonials
  * @route   GET /api/v1/testimonials/
  * @access  Public
  */
-const getAllTestimonials = asyncHandler(async (_, res) => {
+const getAllTestimonials = asyncHandler(async (req, res) => {
+  const testimonials = await Testimonial.find();
 
-  const testimonials = await Testimonial.find()
-  if (!testimonials) {
-    throw new ApiError(404, "Testimonials not found");
+  if (!testimonials.length) {
+    throw new ApiError(404, "No testimonials found");
   }
 
   res.status(200).json(
-    new ApiResponse(
-      200,
-        testimonials,
-      "Testimonials fetched successfully"
-    )
+    new ApiResponse(200, testimonials, "Testimonials fetched successfully")
   );
 });
 
--- src/models/testimonial.model.js
+++ src/models/testimonial.model.js
@@ -1,27 +1,26 @@
 import mongoose, { Schema } from 'mongoose';
 
 const testimonialSchema = new Schema(
-    {
-       username:{
-        type:String,
-        required:true
-       },
-          location:{
-            type:String
-          },
-          message: {
-            type: String,
-            required: true
-          },
-          rating: {
-            type: Number, 
-            min: 1, 
-            max: 5, 
-            required: true
-          },
+  {
+    username:{
+      type:String,
+      required:true
+      
+  },
+    location: {
+      type: String
+    },
+    message: {
+      type: String,
+      required: true
+    },
+    profile:{
+      type:String,
+      default:"https://cdn-icons-png.freepik.com/256/1077/1077012.png?uid=R139182508&ga=GA1.1.1449054835.1702407798&"
+    }
 
-},
-{timstamps:true}
+  },
+  { timstamps: true }
 
 )
-export const Testimonial= mongoose.model('Testimonial', testimonialSchema)
+export const Testimonial = mongoose.model('Testimonial', testimonialSchema)
